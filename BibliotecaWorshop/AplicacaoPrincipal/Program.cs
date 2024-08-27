fc68af0cfa94707de9e41d7b988ec7e6d72d4a02
TatiiNogueira <68739197+TatiiNogueira@users.noreply.github.com>
2024-08-27 16:00:43 +0100

    Add files via upload

---
+++ BibliotecaWorshop/AplicacaoPrincipal/Program.cs
@@ -0,0 +1,30 @@
+﻿//Projeto criado como Console
+
+using Worshop_Acess_Modified; // Importa o namespace do projeto de biblioteca
+
+namespace AplicacaoPrincipal
+{
+    public class Subclasse : ClasseBase
+    {
+        public void AcessarMetodo()
+        {
+            // Acesso permitido porque Subclasse herda de ClasseBase
+            Mensagem();
+        }
+    }
+
+    public class Programa
+    {
+        public static void Main(string[] args)
+        {
+            Subclasse subclasseObj = new Subclasse();
+            // Chama o método protected internal
+            subclasseObj.AcessarMetodo();
+
+            // Tentativa de acesso direto por uma classe que não herda de ClasseBase
+            ClasseBase msgClass = new ClasseBase();
+            // Erro: Não permitido, pois está fora do assembly
+            //msgClass.Mensagem();
+        }
+    }
+}
---
+++ BibliotecaWorshop/BibliotecaWorshop/Class1.cs
@@ -0,0 +1,31 @@
+﻿//Criar Projeto como Library
+//Adcionar nas Dependencias o projeto BibliotecaWorkshop
+
+namespace Worshop_Acess_Modified
+{
+    public class ClasseBase
+    {
+        // Método protegido interno
+        protected internal void Mensagem()
+        {
+            Console.WriteLine("Morango e Banana");
+        }
+
+        // Método protegido interno
+        internal void Mensagem2()
+        {
+            Console.WriteLine("Morango e Banana");
+        }
+    }
+
+    // Classe no mesmo assembly
+    public class ClasseNoMesmoAssembly
+    {
+        public void AcessarMetodo()
+        {
+            ClasseBase msgClass = new ClasseBase();
+            // Acesso permitido porque está no mesmo assembly
+            msgClass.Mensagem();
+        }
+    }
+}
